b34c0b3efe9e69a28420088ba219220cc18435ae
Victor Gil <victor.gil@tenthdimensions.com>
2024-06-11 13:20:42 +0200

    zigbee.h

---
+++ zigbee.h
@@ -0,0 +1,14 @@
+// #if defined(ESP_IDF_COMP_ESPRESSIF__ESP_ZIGBEE_LIB_ENABLED)
+#include "esp_zigbee_attribute.h"
+#include "esp_zigbee_cluster.h"
+#include "esp_zigbee_core.h"
+#include "esp_zigbee_endpoint.h"
+#include "esp_zigbee_ota.h"
+#include "esp_zigbee_secur.h"
+#include "esp_zigbee_trace.h"
+#include "esp_zigbee_type.h"
+// #endif
+// #if defined(ESP_IDF_COMP_ESPRESSIF__ESP_ZBOSS_LIB_ENABLED)
+#include "zboss_api_core.h"
+#include "zboss_api.h"
+// #endif
