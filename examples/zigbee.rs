3a11f23f2de1d0732753264bf0b85ef3cdbe156e
Victor Gil <victor.gil@tenthdimensions.com>
2024-06-11 17:08:04 +0200

    Added radio/host mode

--- examples/zigbee.rs
+++ examples/zigbee.rs
@@ -1,12 +1,17 @@
+use std::default;
+
 use esp_idf_sys::zigbee::{
     esp_zb_platform_config_t,
     esp_zb_platform_config,
-    esp_zb_ncp_config_t,
+    esp_zb_radio_config_t,
     esp_zb_host_config_t,
+    esp_zb_uart_config_t,
     esp_zb_init,
     esp_zb_cfg_s,
     esp_zb_zed_cfg_t,
     esp_zb_cfg_s__bindgen_ty_1,
+    esp_zb_radio_mode_t_ZB_RADIO_MODE_NATIVE,
+    esp_zb_host_connection_mode_t_ZB_HOST_CONNECTION_MODE_NONE,
 };
 use esp_idf_sys::esp_err_t;
 
@@ -15,14 +20,16 @@ fn main() {
 
     unsafe { 
         let mut platform_cfg: esp_zb_platform_config_t = esp_zb_platform_config_t {
-            radio_config: esp_zb_ncp_config_t {
-                radio_mode: 0x0,
+            radio_config: esp_zb_radio_config_t {
+                radio_mode: esp_zb_radio_mode_t_ZB_RADIO_MODE_NATIVE,
+                radio_uart_config: default(esp_zb_uart_config_t),
             },
             host_config: esp_zb_host_config_t {
-                host_mode: 0x02,
+                host_connection_mode: esp_zb_host_connection_mode_t_ZB_HOST_CONNECTION_MODE_NONE,
+                host_uart_config: default(esp_zb_uart_config_t)
             },
         };
-        let err1: esp_err_t = esp_zb_platform_config(&platform_cfg);
+        let err1 = esp_zb_platform_config(&platform_cfg);
 
         let mut cfg: esp_zb_cfg_s = esp_zb_cfg_s{
             esp_zb_role: 0x0, 
@@ -34,7 +41,7 @@ fn main() {
                 },
             },
         };
-        let err2: esp_err_t = esp_zb_init(&mut cfg); 
+        let err2 = esp_zb_init(&mut cfg); 
     };
 
     println!("Complete");
